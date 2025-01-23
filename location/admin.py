6e050ee5cc8c7203e8202df5ba48d4afcc201f4f
Jason Heppler <jason.heppler@gmail.com>
2025-01-23 11:03:24 -0600

    feat(admin): Add admin views for the models

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from unfold.admin import ModelAdmin, StackedInline, TabularInline
+
+from location.models import Location, City, County, State
+
+
+@admin.register(Location)
+class LocationAdmin(ModelAdmin):
+    list_display = ["place_id", "map_name", "state", "city", "lat", "lon"]
+
+
+@admin.register(City)
+class CityAdmin(ModelAdmin):
+    list_display = ["name", "state", "county"]
+
+
+@admin.register(County)
+class CountyAdmin(ModelAdmin):
+    list_display = ["name", "state"]
+
+
+@admin.register(State)
+class StateAdmin(ModelAdmin):
+    list_display = ["name", "abbreviation"]
