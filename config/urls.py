3362a0c6360d50d4f38857183b649fb030300b31
qtrinh2 <qtrinh2@gmu.edu>
2026-02-03 11:23:14 -0500

    updating debug toolbar to be more consistent with other django projects

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
-import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from religious_ecologies.views import index
 
@@ -23,6 +24,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [path("__debug__/", include(debug_toolbar_urls()))] + urlpatterns
     # Serve static files in development
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
